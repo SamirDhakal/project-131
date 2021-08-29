2ec3e9585633f599dcbc5eb0dd0494c0b644cbdf
SamirDhakal <68449594+SamirDhakal@users.noreply.github.com>
2021-08-28 21:03:32 -0400

    Update grav.py

--- grav.py
+++ grav.py
@@ -11,7 +11,24 @@ headers = rows[0]
 star_data_rows = rows[1:]
 print(headers)
 print(star_data_rows[0])
+mass = []
+radius = []
+star_names = []
+for star_data in star_data_rows:
+  mass.append(star_data[3])
+  radius.append(star_data[4])
+  star_names.append(star_data[1])
 
-star_mass = []
+def convert_to_si(radius,mass): 
+  for i in range(0,len(radius)-1): 
+    radius[i] = radius[i]*6.957e+8 
+    mass[i] = mass[i]*1.989e+30 
+    convert_to_si(radius,mass)
 
-print(star_mass)
+star_gravity = []
+for index, name in enumerate(star_names):
+  gravity = (float(mass[index])) / (float(radius[index])*float(radius[index]))
+  star_gravity.append(gravity)
+
+convert_to_si(radius, mass)
+print(star_gravity)
